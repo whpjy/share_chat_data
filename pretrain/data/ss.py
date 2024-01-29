6fb886bae5bbfe5229a5f12ac48701e98705680d
wanghang <2656743024@qq.com>
2024-01-29 14:54:13 +0800

    001

--- pretrain/data/ss.py
+++ pretrain/data/ss.py
@@ -4,12 +4,21 @@ filename = "zhibiao.txt"
 
 # 使用with语句打开文件，确保在操作完成后自动关闭
 with open(filename, 'r', encoding='utf-8') as f:
-
     json_data = json.load(f)
 
 print(len(json_data))
 
-target2id = {}
+new_json_all = {}
+
 for single_data in json_data:
+
+    new_group = []
     for group_word in single_data['group']:
-        print(group_word)
+        new_group.append({group_word["columnName"]: group_word})
+
+    single_data['group'] = new_group
+    new_json_all[single_data["targetName"]] = single_data
+
+filename_json = "zhibiao.json"
+fr = open(filename_json, 'w', encoding='utf-8')
+json.dump(new_json_all, fr, ensure_ascii=False, indent=4)
